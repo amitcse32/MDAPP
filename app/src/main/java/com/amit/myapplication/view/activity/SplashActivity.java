9da5a396939f0c9ddd849995063ee4d2e7e2879b
Priyanka <priyankathakur@csgroupchd.chd>
2017-02-18 13:21:30 +0530

    #Splash Screen Load Start

---
+++ app/src/main/java/com/amit/myapplication/view/activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.amit.myapplication.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.amit.myapplication.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+    //Splash Screen Load Start
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
