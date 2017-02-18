10c83de5be744f288c0bda6a1d9e492f5e6f9bf7
Priyanka <priyankathakur@csgroupchd.chd>
2017-02-18 13:45:37 +0530

    #Check internet connection on splash screen.

---
+++ app/src/main/java/com/amit/myapplication/utils/customcontrols/dialogs/ApplicationDialogs.java
@@ -0,0 +1,52 @@
+package com.amit.myapplication.utils.customcontrols.dialogs;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import com.amit.myapplication.R;
+import com.amit.myapplication.view.activity.SplashActivity;
+
+/**
+ * Created by Mobile on 2/18/17.
+ */
+
+public class ApplicationDialogs {
+
+
+
+    // Show dialog and close on press of OK
+
+    public void showMessageDialogWithFinish(final Activity activity, String message) {
+
+        AlertDialog.Builder dialog=new AlertDialog.Builder(activity);
+        dialog.setCancelable(false);
+        dialog.setMessage(message);
+        dialog.setTitle(activity.getString(R.string.feedback));
+        dialog.setPositiveButton(activity.getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            activity.finish();
+            }
+        });
+        dialog.show();
+
+    }
+
+
+    // Show dialog and keep activity running on press of ok
+    public void showMessageDialog(Activity activity,String message)
+    {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(activity);
+        dialog.setCancelable(false);
+        dialog.setMessage(message);
+        dialog.setTitle(activity.getString(R.string.feedback));
+        dialog.setPositiveButton(activity.getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
+    }
+}
--- app/src/main/java/com/amit/myapplication/view/activity/SplashActivity.java
+++ app/src/main/java/com/amit/myapplication/view/activity/SplashActivity.java
@@ -3,6 +3,7 @@ package com.amit.myapplication.view.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.amit.myapplication.R;
+import com.amit.myapplication.utils.customcontrols.dialogs.ApplicationDialogs;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,4 +15,23 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkConnection();
+    }
+
+    //Check internet connetion
+    private void checkConnection()
+    {
+
+
+        //Show dialog if there is no internet connection
+        ApplicationDialogs applicationDialogs=new ApplicationDialogs();
+        applicationDialogs.showMessageDialogWithFinish(this,getString(R.string.internetconnectionmessage));
+
+    }
+
 }
